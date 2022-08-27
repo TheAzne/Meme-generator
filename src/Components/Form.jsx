bbc800d76e1332a4ecc02058496945d82552ca29
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2022-08-27 16:36:47 +0200

    update

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import memesData from "../memesData";
 
 export default () => {
+    event.preventDefault();
+
+    const [memeImage, setMemeImage] = React.useState("");
 
 
-    function getMemeImg(){
-       const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        
-    }
+
+
+  function getMemeImg() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
 
   return (
     <main>
       <form className="form">
         <input type="text" placeholder="Top text" className="form-input" />
         <input type="text" placeholder="Bottom text" className="form-input" />
-        <button onClick={getMemeImg} className="form-button">Get a new meme image</button>
+        <button onClick={getMemeImg} className="form-button">
+          Get a new meme image
+        </button>
       </form>
+      <img src={memeImage} />
     </main>
   );
 };
